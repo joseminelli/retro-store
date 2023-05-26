ec05e55f329d83430eb815d295c7b74e01087399
homem do acre <zecarminelli@gmail.com>
2023-05-26 13:10:23 -0300

    final commit

--- scripts/detalhes.js
+++ scripts/detalhes.js
@@ -15,29 +15,34 @@ function readJSON(file, callback) {
 function displayItem(item) {
   var container = document.getElementById("item-container");
 
-   // Cria o botão para voltar para o index.html
-   var backButton = document.createElement("button");
-   backButton.setAttribute("id", "voltar");
-   backButton.addEventListener("click", function() {
-     window.location.href = "index.html";
-   });
+  // Cria o botão para voltar para o index.html
+  var backButton = document.createElement("button");
+  backButton.setAttribute("id", "voltar");
+  backButton.addEventListener("click", function() {
+    window.location.href = "index.html";
+  });
 
-   backButton.ontouchstart = function () {
+  backButton.ontouchstart = function () {
     window.location.href = "index.html";
   };
- 
-   var btn = document.createElement("button");
-   btn.classList.add("btn");
-   btn.textContent = "Adicionar ao carrinho";
 
-   btn.addEventListener("click", function() {
+  var btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.textContent = "Adicionar ao carrinho";
+
+  btn.addEventListener("click", function() {
     alert("Este item não está em estoque no momento :(");
   });
-   
-   // Cria a imagem de seta para o botão
-   var arrowImage = document.createElement("img");
-   arrowImage.src = "https://cdn.discordapp.com/attachments/1109149984357613638/1111653810511822868/arrow.png";
-   backButton.appendChild(arrowImage);
+
+  // Cria a imagem de seta para o botão
+  var arrowImage = document.createElement("img");
+  arrowImage.src = "https://cdn.discordapp.com/attachments/1109149984357613638/1111653810511822868/arrow.png";
+  backButton.appendChild(arrowImage);
+
+  // Cria a logo
+  var logoImage = document.createElement("img");
+  logoImage.setAttribute("id", "logo");
+  logoImage.src = "img/retrostore.png";
 
   // Cria elementos HTML para exibir as informações do item
   var title = document.createElement("h1");
@@ -55,12 +60,14 @@ function displayItem(item) {
 
   // Adiciona os elementos criados ao container
   container.appendChild(backButton);
+  container.appendChild(logoImage);
   container.appendChild(title);
   container.appendChild(image);
   container.appendChild(price);
   container.appendChild(description);
   container.appendChild(btn);
 }
+
 
 // Obtém o valor do itemId da URL
 var url = window.location.href;
